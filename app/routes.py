886a5749e8862aa07d6c7af6e2be259a62c0d1be
IgorLoskutov <40142446+IgorLoskutov@users.noreply.github.com>
2019-04-15 10:01:25 +0300

    search added, pagination corrected

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from flask import render_template, request
 from app import app, db
 from app.models import Employee
 
+from sqlalchemy import func
+
+import math
+
 
 @app.route('/')
 @app.route('/index')
@@ -35,11 +39,12 @@ def index():
 def table():
     template = 'table.html'
     sort_key = 'full_name' #default sort column if not set in request
-    limit = 25 #default limit if not set in request
+    limit = 10 #default limit if not set in request
     offset = 0 #default offset
-    page = 5
+    page_n = 5
+    current_page = 1
 
-    sort_keys={
+    sort_keys={     #not keeping Employee fields names on front
         'id':'id',
         'Manager id':'manager_id',
         'Name':'full_name',
@@ -55,20 +60,53 @@ def table():
             sort_key=sort_keys[request.args['sort']]
         if 'page' in request.args:
             try:
-                page = int(request.args['page'])
-                offset = (page - 1) * limit #pagination of the output
+                current_page = int(request.args['page'])
+                offset = (current_page - 1) * limit #pagination of the output
+                page_n = limit
             except ValueError:
                 pass
         limit += offset
         template = 'rows.html'
 
-    emps = Employee().query.order_by(sort_key)[offset:limit]
+    if request.args and 'find' in request.args and request.args['find']!= '':
+        '''search 'find' input through all VARCHAR, INT, NUM fields
+        whatever column names are'''
+        find_str = "%{}%".format(request.args['find'])
+        find = request.args['find']
+        emps = Employee.query.limit(0) #empty query to "append" UNION SELECT
+        for c in Employee.__table__.c:
+            if 'VARCHAR' in str(c.type):
+                emps = emps.union(
+                    Employee.query.filter(
+                        Employee.__dict__[c.name].like(find_str)
+                        )
+                    )
+            elif 'INT' in str(c.type) or 'NUM' in str(c.type):
+                try:
+                    find = int(find)
+                    emps = emps.union(
+                        Employee.query.filter(
+                            Employee.__dict__[c.name]==find
+                            )
+                        )
+                except :
+                    pass
+    else:
+        emps = Employee().query
 
-    if page < 4:
-        page = 4
+    pages = math.ceil(emps.count()/page_n)
+
+    if pages <= 8:
+        pages = (1, pages)
+    elif current_page>=pages-3:
+        pages = (pages-7, pages)
+    else:
+        pages = (current_page-3, max(current_page+4, 8))
+
+    emps = emps.order_by(Employee.__dict__[sort_key])[offset:limit]
 
     return render_template(
         template,
         emps = emps,
-        page = page
+        page = pages
         )
